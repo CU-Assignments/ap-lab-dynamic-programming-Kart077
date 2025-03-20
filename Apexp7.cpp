f9035585e6bff7f360a0e267921fe8543308cd7f
Kartik sharma <122304116+Kart077@users.noreply.github.com>
2025-03-20 14:56:59 +0530

    Add files via upload

---
+++ Apexp7.cpp
@@ -0,0 +1,78 @@
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> minCoins(amount + 1, amount + 1);
+        minCoins[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0) {
+                    minCoins[i] = min(minCoins[i], 1 + minCoins[i - coins[j]]);
+                }
+            }
+        }
+
+        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
+    }
+};
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int cur = 0;
+
+        for (int i = 3; i < n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;        
+    }
+};
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+};
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+};
+
